297014aedd52b1dd9e9854f1b019e1d8fb1fb2f3
wdcs-harshitPadalia <Harshit.padalia@webcluesinfotech.com>
2022-12-21 10:49:54 +0530

    updated async storage for web support

--- App.js
+++ App.js
@@ -1,45 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import {
-	Alert,
-	Button,
-	Image,
-	Platform,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	View,
-  StyleSheet
-} from 'react-native';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {persistStore} from 'redux-persist';
-import store from './src/redux/store';
-import Routes from './src/navigation/routes';
-import {ThemeProvider} from './src/theme/createTheme';
+  Alert,
+  Button,
+  Image,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+} from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import store from "./src/redux/store";
+import Routes from "./src/navigation/routes";
+import { ThemeProvider } from "./src/theme/createTheme";
 import {
-	QrcodeModal,
-	RenderQrcodeModalProps,
-	useWalletConnect,
-	WalletService,
-	withWalletConnect
-} from '@walletconnect/react-native-dapp';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import NoInternetView from './src/components/NoInternetView';
-import {PortalProvider, enableLogging} from '@gorhom/portal';
-import KeyboardManager from 'react-native-keyboard-manager';
-import NotificationManager from './src/notificationManager';
-import FlashMessage from 'react-native-flash-message';
+  QrcodeModal,
+  RenderQrcodeModalProps,
+  useWalletConnect,
+  WalletService,
+  withWalletConnect,
+} from "@walletconnect/react-native-dapp";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NoInternetView from "./src/components/NoInternetView";
+import { PortalProvider, enableLogging } from "@gorhom/portal";
+import KeyboardManager from "react-native-keyboard-manager";
+import NotificationManager from "./src/notificationManager";
+import FlashMessage from "react-native-flash-message";
 import {
-	API_REGIONS,
-	createClient,
-	disableCache,
-	enableCache
-} from '@amityco/ts-sdk';
-import {defaultTheme} from './src/theme/defaultTheme';
-import CustomWalletBottomSheet from './src/components/CustomWalletBottomSheet';
-import {AppSchema} from './src/constants/api';
-import {initSmartLookSession} from './src/components/SmartLookSDKHelper';
-import { useEffect } from 'react';
+  API_REGIONS,
+  createClient,
+  disableCache,
+  enableCache,
+} from "@amityco/ts-sdk";
+import { defaultTheme } from "./src/theme/defaultTheme";
+import CustomWalletBottomSheet from "./src/components/CustomWalletBottomSheet";
+import { AppSchema } from "./src/constants/api";
+import { initSmartLookSession } from "./src/components/SmartLookSDKHelper";
+import { useEffect } from "react";
 // export default () => {
 //   	<Provider store={store}>
 //   <StatusBar
@@ -55,23 +55,25 @@ import { useEffect } from 'react';
 //   {/* </PersistGate> */}
 //   <FlashMessage position="top" />
 // </Provider>
-  
-// }
 
+// }
+let persistor = persistStore(store);
 
 export default () => (
-  <View style={{backgroundColor: 'red', flex: 1}}>
-  	<Provider store={store}>
-    <Routes />
-    </Provider>
+  <View style={{ backgroundColor: "red", flex: 1 }}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </PersistGate>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

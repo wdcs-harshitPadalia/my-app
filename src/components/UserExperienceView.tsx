c87b198d2a48b0df21c6af752956667c5df73e8a
krunalsatapara <104752593+wdcs-krunalsatapara@users.noreply.github.com>
2022-12-28 11:32:28 +0530

    PWA/KRUNAL (#4)
    * fix web view alternative for web
    
    * resolve bet win loss gradient text
    
    * fix token balance issue
    
    * fix web view issue and fix setup profile birthday issue, and fix switch and checkbox issue
    
    * Delete yarn.lock
    
    remove file yarn.lock
    
    * Delete index.js
    
    delete index.js
    
    * Update index.tsx
    
    update login index file
    
    * PWA - Profile Module
    - ui changes in profile screen
    - ui changes in wallet screen
    - ui changes in wallet stats screen
    
    * set condition for os wise
    
    * remove log

--- src/components/UserExperienceView.tsx
+++ src/components/UserExperienceView.tsx
@@ -1,161 +1,201 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
-import {TextInputProps, View} from 'react-native';
-import ExpoFastImage from 'expo-fast-image';
-import {getLevelRank} from '../constants/utils/Function';
-import {Fonts, moderateScale, verticalScale} from '../theme';
-import colors from '../theme/colors';
-import {defaultTheme} from '../theme/defaultTheme';
-import BetsProgress from './BetsProgress';
-import CircularProgress from 'react-native-circular-progress-indicator';
-import Strings from '../constants/strings';
+import React from "react";
+import {
+  Dimensions,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInputProps,
+  View,
+} from "react-native";
+
+import ExpoFastImage from "expo-fast-image";
+import CircularProgress from "react-native-circular-progress-indicator";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+
+import { Fonts, moderateScale, verticalScale } from "../theme";
+import colors from "../theme/colors";
+import { defaultTheme } from "../theme/defaultTheme";
+
+import Strings from "../constants/strings";
+import { getLevelRank } from "../constants/utils/Function";
+
+import BetsProgress from "./BetsProgress";
 
 interface Props extends TextInputProps {
-	userLevel: number;
-	currentFees: number;
+  userLevel: number;
+  currentFees: number;
 
-	userLevelMinBetValue?: number;
-	userLevelMaxBetValue?: number;
-	totalBetsCount?: number;
+  userLevelMinBetValue?: number;
+  userLevelMaxBetValue?: number;
+  totalBetsCount?: number;
 
-	progressTitle?: string;
-	nextGoalTitle?: string;
-	currentFeesTitle?: string;
+  progressTitle?: string;
+  nextGoalTitle?: string;
+  currentFeesTitle?: string;
 }
 
-const UserExperienceView: React.FC<Props> = props => {
-	const {
-		userLevel,
-		currentFees,
-		userLevelMinBetValue,
-		userLevelMaxBetValue,
-		totalBetsCount,
-		progressTitle,
-		nextGoalTitle,
-		currentFeesTitle
-	} = props;
+const UserExperienceView: React.FC<Props> = (props) => {
+  const {
+    userLevel,
+    currentFees,
+    userLevelMinBetValue,
+    userLevelMaxBetValue,
+    totalBetsCount,
+    progressTitle,
+    nextGoalTitle,
+    currentFeesTitle,
+  } = props;
 
-	// function to calculate the progress of the user according to their level.
-	function userProgress(level: number, minValue: number, maxValue: number) {
-		let totalValue = maxValue - minValue;
-		if (userLevel === 4) {
-			return 50;
-		} else if (userLevel === 0) {
-			return (level * 50) / totalValue;
-		} else {
-			let totalLevel = level - minValue + 1;
-			return (totalLevel * 50) / totalValue;
-		}
-	}
+  // function to calculate the progress of the user according to their level.
+  function userProgress(level: number, minValue: number, maxValue: number) {
+    let totalValue = maxValue - minValue;
+    if (userLevel === 4) {
+      return 50;
+    } else if (userLevel === 0) {
+      return (level * 50) / totalValue;
+    } else {
+      let totalLevel = level - minValue + 1;
+      return (totalLevel * 50) / totalValue;
+    }
+  }
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.titleText}>{Strings.my_experience}</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>{Strings.my_experience}</Text>
 
-			<View style={styles.progressView}>
-				<View style={styles.circularView}>
-					<CircularProgress
-						value={userProgress(
-							totalBetsCount,
-							userLevelMinBetValue,
-							userLevelMaxBetValue
-						)}
-						radius={110}
-						activeStrokeWidth={totalBetsCount === userLevelMinBetValue ? 0 : 24}
-						inActiveStrokeWidth={24}
-						progressValueStyle={{fontWeight: '100', color: '#ffffff00'}}
-						inActiveStrokeColor={defaultTheme.backGroundColor}
-						activeStrokeColor={colors.darkRed}
-						activeStrokeSecondaryColor={colors.purple}
-					/>
-				</View>
-			</View>
+      <View style={styles.progressView}>
+        <View style={styles.circularView}>
+          {Platform.OS === "web" ? (
+            <AnimatedCircularProgress
+              size={180}
+              width={15}
+              fill={userProgress(
+                totalBetsCount * 2,
+                userLevelMinBetValue,
+                userLevelMaxBetValue
+              )}
+              tintColor={colors.darkRed}
+              backgroundColor={defaultTheme.backGroundColor}
+              arcSweepAngle={180}
+              rotation={270}
+              lineCap="round"
+            />
+          ) : (
+            <CircularProgress
+              value={userProgress(
+                totalBetsCount,
+                userLevelMinBetValue,
+                userLevelMaxBetValue
+              )}
+              radius={110}
+              activeStrokeWidth={
+                totalBetsCount === userLevelMinBetValue ? 0 : 24
+              }
+              inActiveStrokeWidth={24}
+              progressValueStyle={{ fontWeight: "100", color: "#ffffff00" }}
+              inActiveStrokeColor={defaultTheme.backGroundColor}
+              activeStrokeColor={colors.darkRed}
+              activeStrokeSecondaryColor={colors.purple}
+            />
+          )}
+        </View>
+      </View>
 
-			<ExpoFastImage
-				style={styles.viewBadgeStyle}
-				resizeMode="cover"
-				source={getLevelRank(userLevel)?.image}
-			/>
-			<View>
-				<Text style={styles.userLevelText}>{getLevelRank(userLevel).type}</Text>
-			</View>
-			<View style={styles.progressIndicatorValueView}>
-				<Text style={[styles.progressIndicatorValueText, {marginRight: 60}]}>
-					{userLevelMinBetValue}
-				</Text>
-				<Text style={[styles.progressIndicatorValueText, {marginLeft: 60}]}>
-					{userLevelMaxBetValue}
-				</Text>
-			</View>
+      <ExpoFastImage
+        style={styles.viewBadgeStyle}
+        resizeMode="cover"
+        source={getLevelRank(userLevel)?.image}
+      />
+      <View>
+        <Text style={styles.userLevelText}>{getLevelRank(userLevel).type}</Text>
+      </View>
+      <View style={styles.progressIndicatorValueView}>
+        <Text
+          style={[
+            styles.progressIndicatorValueText,
+            { marginRight: Platform.OS === "web" ? 0 : 60 },
+          ]}
+        >
+          {userLevelMinBetValue}
+        </Text>
+        <Text
+          style={[
+            styles.progressIndicatorValueText,
+            { marginLeft: Platform.OS === "web" ? 0 : 60 },
+          ]}
+        >
+          {userLevelMaxBetValue}
+        </Text>
+      </View>
 
-			<View style={styles.betView}>
-				<BetsProgress
-					betsLost={userLevelMaxBetValue}
-					betsPending={currentFees}
-					betsWon={totalBetsCount}
-					progressTitle={progressTitle}
-					nextGoalTitle={nextGoalTitle}
-					currentFeesTitle={currentFeesTitle}
-					betLossPercent={0}
-					betWonPercent={0}
-					isGradiantColorVisible={true}
-				/>
-			</View>
-		</View>
-	);
+      <View style={styles.betView}>
+        <BetsProgress
+          betsLost={userLevelMaxBetValue}
+          betsPending={currentFees}
+          betsWon={totalBetsCount}
+          progressTitle={progressTitle}
+          nextGoalTitle={nextGoalTitle}
+          currentFeesTitle={currentFeesTitle}
+          betLossPercent={0}
+          betWonPercent={0}
+          isGradiantColorVisible={true}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default UserExperienceView;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: colors.black,
-		borderRadius: verticalScale(8)
-	},
-	titleText: {
-		fontFamily: Fonts.type.Krona_Regular,
-		fontSize: moderateScale(18),
-		color: colors.white,
-		margin: verticalScale(20),
-		textAlign: 'center'
-	},
-	progressView: {
-		alignItems: 'center',
-		marginTop: verticalScale(10),
-		height: 110,
-		overflow: 'hidden'
-	},
-	circularView: {
-		height: 220,
-		transform: [{rotate: '-90deg'}]
-	},
-	viewBadgeStyle: {
-		marginTop: verticalScale(-35),
-		alignSelf: 'center',
-		width: 60,
-		height: 60
-	},
-	progressIndicatorValueView: {
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-		marginTop: -14
-	},
-	progressIndicatorValueText: {
-		fontFamily: Fonts.type.Inter_Bold,
-		fontSize: moderateScale(12),
-		color: colors.white
-	},
-	userLevelText: {
-		fontFamily: Fonts.type.Inter_Bold,
-		fontSize: moderateScale(12),
-		color: colors.white,
-		opacity: 0.7,
-		textAlign: 'center',
-		marginTop: 11
-	},
-	betView: {
-		marginTop: 16,
-		marginBottom: 20
-	}
+  container: {
+    backgroundColor: colors.black,
+    borderRadius: verticalScale(8),
+  },
+  titleText: {
+    fontFamily: Fonts.type.Krona_Regular,
+    fontSize: moderateScale(18),
+    color: colors.white,
+    margin: verticalScale(20),
+    textAlign: "center",
+  },
+  progressView: {
+    alignItems: "center",
+    marginTop: verticalScale(10),
+    height: 110,
+    overflow: "hidden",
+  },
+  circularView: {
+    height: Platform.OS !== "web" && 220,
+    transform: [{ rotate: Platform.OS !== "web" && "-90deg" }],
+  },
+  viewBadgeStyle: {
+    marginTop: verticalScale(Platform.OS === "web" ? -40 : -35),
+    alignSelf: "center",
+    width: 60,
+    height: 60,
+  },
+  progressIndicatorValueView: {
+    flexDirection: "row",
+    justifyContent: Platform.OS === "web" ? "space-around" : "space-evenly",
+    marginTop: -14,
+  },
+  progressIndicatorValueText: {
+    fontFamily: Fonts.type.Inter_Bold,
+    fontSize: moderateScale(12),
+    color: colors.white,
+  },
+  userLevelText: {
+    fontFamily: Fonts.type.Inter_Bold,
+    fontSize: moderateScale(12),
+    color: colors.white,
+    opacity: 0.7,
+    textAlign: "center",
+    marginTop: 11,
+  },
+  betView: {
+    marginTop: 16,
+    marginBottom: 20,
+  },
 });
--- src/screens/PostLogin/ProfileScreen/style.tsx
+++ src/screens/PostLogin/ProfileScreen/style.tsx
@@ -1,107 +1,109 @@
 /* eslint-disable prettier/prettier */
-import {Platform, StyleSheet} from 'react-native';
+import { Platform, StyleSheet } from "react-native";
 import {
-	Fonts,
-	horizontalScale,
-	moderateScale,
-	verticalScale
-} from '../../../theme';
-import colors from '../../../theme/colors';
-import {defaultTheme} from '../../../theme/defaultTheme';
+  Fonts,
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from "../../../theme";
+import colors from "../../../theme/colors";
+import { defaultTheme } from "../../../theme/defaultTheme";
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: defaultTheme.backGroundColor,
-		marginBottom: Platform.OS === 'ios' ? 0 : horizontalScale(20)
-	},
-	viewContain: {
-		marginTop: verticalScale(20),
-		marginHorizontal: horizontalScale(16),
-		flexDirection: 'column'
-	},
-	nameStyle: {
-		fontSize: moderateScale(33),
-		color: colors.white,
-		fontFamily: Fonts.type.Krona_Regular,
-		textAlign: 'center'
-	},
-	userNameStyle: {
-		fontSize: moderateScale(18),
-		color: colors.white,
-		fontFamily: Fonts.type.Krona_Regular,
-		textAlign: 'center'
-	},
-	noobNameStyle: {
-		fontSize: moderateScale(14),
-		color: colors.white,
-		fontFamily: Fonts.type.Krona_Regular,
-		textAlign: 'center',
-		marginTop: verticalScale(4)
-	},
-	bioGraphyStyle: {
-		fontSize: moderateScale(12),
-		color: colors.textTitle,
-		fontFamily: Fonts.type.Inter_Regular,
-		textAlign: 'center',
-		marginTop: verticalScale(20)
-	},
-	viewUserInfo: {
-		borderRadius: verticalScale(10),
-		marginTop: verticalScale(16)
-	},
-	userGroupStyle: {
-		marginTop: verticalScale(16),
-		overflow: 'hidden'
-	},
-	userBetsViewStyle: {
-		marginTop: verticalScale(16),
-		marginBottom: verticalScale(16)
-	},
-	viewChart: {
-		backgroundColor: defaultTheme.secondaryBackGroundColor,
-		borderRadius: verticalScale(10),
-		marginTop: verticalScale(16),
-		paddingBottom: verticalScale(16),
-		marginBottom: verticalScale(16),
-	},
-	statisticsTitleStyle: {
-		fontSize: moderateScale(18),
-		color: colors.white,
-		fontFamily: Fonts.type.Krona_Regular,
-		textAlign: 'center',
-		margin: verticalScale(16)
-	},
-	categoryStyle: {
-		fontSize: moderateScale(12),
-		color: colors.white,
-		fontFamily: Fonts.type.Inter_Bold,
-		textAlign: 'left',
-		margin: verticalScale(16)
-	},
-	currencyStyle: {
-		// marginTop: verticalScale(16),
-		marginBottom: verticalScale(16)
-	},
-	betsStyle: {
-		marginTop: verticalScale(8)
-		// marginBottom: verticalScale(16),
-	},
-	viewMoreBetStyle: {
-		flex: 1,
-		flexDirection: 'row',
-		justifyContent: 'flex-end'
-	},
-	viewMoreTestStyle: {
-		color: colors.white,
-		fontSize: moderateScale(12),
-		fontFamily: Fonts.type.Krona_Regular,
-		marginLeft: horizontalScale(14),
-		marginBottom: verticalScale(24)
-	},
-	loginButtonSocial: {
-		marginTop: verticalScale(16),
-		borderRadius: verticalScale(50),
-		overflow: 'hidden'
-	}
+  container: {
+    flex: 1,
+    backgroundColor: defaultTheme.backGroundColor,
+    marginBottom: Platform.OS === "ios" ? 0 : horizontalScale(20),
+  },
+  viewContain: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(16),
+    flexDirection: "column",
+  },
+  nameStyle: {
+    fontSize: moderateScale(33),
+    color: colors.white,
+    fontFamily: Fonts.type.Krona_Regular,
+    textAlign: "center",
+  },
+  userNameStyle: {
+    fontSize: moderateScale(18),
+    color: colors.white,
+    fontFamily: Fonts.type.Krona_Regular,
+    textAlign: "center",
+  },
+  noobNameStyle: {
+    fontSize: moderateScale(14),
+    color: colors.white,
+    fontFamily: Fonts.type.Krona_Regular,
+    textAlign: "center",
+    marginTop: verticalScale(4),
+  },
+  bioGraphyStyle: {
+    fontSize: moderateScale(12),
+    color: colors.textTitle,
+    fontFamily: Fonts.type.Inter_Regular,
+    textAlign: "center",
+    marginTop: verticalScale(20),
+  },
+  viewUserInfo: {
+    borderRadius: verticalScale(10),
+    marginTop: verticalScale(16),
+  },
+  userGroupStyle: {
+    marginTop: verticalScale(16),
+    overflow: "hidden",
+  },
+  userBetsViewStyle: {
+    marginTop: verticalScale(16),
+    marginBottom: verticalScale(16),
+  },
+  viewChart: {
+    backgroundColor: defaultTheme.secondaryBackGroundColor,
+    borderRadius: verticalScale(10),
+    marginTop: verticalScale(16),
+    paddingBottom: verticalScale(16),
+    marginBottom: verticalScale(16),
+  },
+  statisticsTitleStyle: {
+    fontSize: moderateScale(18),
+    color: colors.white,
+    fontFamily: Fonts.type.Krona_Regular,
+    textAlign: "center",
+    margin: verticalScale(16),
+  },
+  categoryStyle: {
+    fontSize: moderateScale(12),
+    color: colors.white,
+    fontFamily: Fonts.type.Inter_Bold,
+    textAlign: "left",
+    marginHorizontal: verticalScale(16),
+    marginTop: verticalScale(16),
+    marginBottom: verticalScale(Platform.OS === "web" ? 0 : 16),
+  },
+  currencyStyle: {
+    // marginTop: verticalScale(16),
+    marginBottom: verticalScale(16),
+  },
+  betsStyle: {
+    marginTop: verticalScale(8),
+    // marginBottom: verticalScale(16),
+  },
+  viewMoreBetStyle: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  viewMoreTestStyle: {
+    color: colors.white,
+    fontSize: moderateScale(12),
+    fontFamily: Fonts.type.Krona_Regular,
+    marginLeft: horizontalScale(14),
+    marginBottom: verticalScale(24),
+  },
+  loginButtonSocial: {
+    marginTop: verticalScale(16),
+    borderRadius: verticalScale(50),
+    overflow: "hidden",
+  },
 });
 export default styles;
